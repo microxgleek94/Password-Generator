02c16facf934e334081f90240284cad96d36a24a
Keiana Hanson <kiki_hanson@aol.com>
2020-05-25 21:16:33 -0400

    Added if statements, now debugging

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
   var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+  function writePassword() {
 
 //User will select the length of their password
 // while loop is used to make sure the user picks a # between 8 and 128
@@ -11,7 +13,8 @@
 }
     // checks user length input
     console.log(`User length input: ${length}`);
-    
+}
+
 // Below prompts are used to confirm which characters the user wants to use in password 
 var userNumber = confirm("Do you want to include numbers?");
 var userSpecial = confirm("Do you want to include special characters?");
@@ -25,23 +28,20 @@ var userUpper = confirm("Do you want to include uppercase characters?");
   console.log(`userUpper input ${userUpper}`);
 
 
-
 //This calls the generatePassword function
 var password = generatePassword();
-
 
 
 // This function will create a random password based on user input
 function generatePassword() {
 
  // defining the variables that will create the random password
-var passwordLen = "0";
-
 var numbers = "0123456789"; 
 var specials = "@!#$%^&*()_+"; 
 var lowers = "abcdefghijklmnopqrstuvwxyz"; 
 var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 var randomPasswordLen = "";
+var password="";
 
   //checks variables
   console.log(`Password length: ${passwordLen}`);
@@ -51,32 +51,50 @@ var randomPasswordLen = "";
   console.log(`Uppers: ${uppers}`);
   console.log(`RandomLen: ${randomPasswordLen}`);
 
-
-for (var i = 0; i < length; i++ ) {
-    randomPasswordLen += randomPasswordLen.charAt(Math.floor(Math.random() * randomPasswordLen));
-    console.log(passwordLen);
-  }
-
-
+  // if the user's input is true, that value will be added to
+//var randomPasswordLen empty string;
 if (userNumber === true) {
-  // add numbers to password
+  randomPasswordLen = randomPasswordLen.concat(numbers);
 }
 if (userSpecial === true) {
-  // add special characters to password
+  randomPasswordLen = randomPasswordLen.concat(specials);
 }
 if (userLower === true) {
-  // add lowercase characters to password
+  randomPasswordLen = randomPasswordLen.concat(lowers);
 }
 if (userUpper === true) {
-  // add uppercase letters to password
+  randomPasswordLen = randomPasswordLen.concat(uppers);
 }
 else {
   alert("You need to pick at least one character type.");
 }
 
-  return randomPasswordLen;
+// checks to see random characters chosen
+console.log("---------");
+console.log(`Random Numbers: ${randomPasswordLen.concat(numbers)}`);
+console.log("---------");
+console.log(`Random Specials: ${randomPasswordLen.concat(specials)}`);
+console.log("---------");
+console.log(`Random Lowers: ${randomPasswordLen.concat(lowers)}`);
+console.log("---------");
+console.log(`Random Uppers: ${randomPasswordLen.concat(uppers)}`);
+
+
+
+// for loop is used to pull a character out of the string
+// built from the user's input
+for (var i = 0; i < randomPasswordLen.length; i++) {
+    randomPasswordLen = randomPasswordLen.charAt(Math.floor(Math.random() * randomPasswordLen));
+   
+    // the random characters chosen are added to an empty string
+    // and returned to be posted to textarea
+    randomPasswordLen+= password
+    console.log(password);
+  }
+  return password
 
 } // this is the end of the generatePassword function
+
 
 
 // Add event listener to generate button
